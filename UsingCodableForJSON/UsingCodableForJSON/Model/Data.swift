47498c183585c854a4ecca0bf1843348a8d2d2f7
cuongdd <duycuongkma@gmail.com>
2025-02-24 18:08:06 +0700

    add model file and ui show list

---
+++ UsingCodableForJSON/UsingCodableForJSON/Model/Data.swift
@@ -0,0 +1,9 @@
+//
+//  Data.swift
+//  UsingCodableForJSON
+//
+//  Created by cuongdd on 24/2/25.
+//  Copyright © 2025 Ngô Bảo Châu. All rights reserved.
+//
+
+import Foundation
---
+++ UsingCodableForJSON/UsingCodableForJSON/Model/Json4Swift_Base.swift
@@ -0,0 +1,9 @@
+//
+//  Json4Swift_Base.swift
+//  UsingCodableForJSON
+//
+//  Created by cuongdd on 24/2/25.
+//  Copyright © 2025 Ngô Bảo Châu. All rights reserved.
+//
+
+import Foundation
